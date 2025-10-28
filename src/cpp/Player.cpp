e27a8bedb4e670b8cc7ea0f2bd609dfd0d7eb384
JMG3N0 <joan.rivas.cepeda@gmail.com>
2025-10-28 20:27:42 -0300

    Fixed acceleration

--- src/cpp/Player.cpp
+++ src/cpp/Player.cpp
@@ -16,9 +16,9 @@ namespace space_ore
 		player.hp = 10;
 		player.score = 0;
 		player.maxScore = 0;
-		player.speed = 0.0f;
+		player.speed = { 0.0f, 0.0f };
 		player.maxSpeed = 100.0f;
-		player.acceleration = 30.0f;
+		player.currentSpeed = 30.0f;
 		player.pivot.x = 16.0f;
 		player.pivot.y = 16.0f;
 		player.rotation = 0.0f;
@@ -37,9 +37,9 @@ namespace space_ore
 		player.currentBullets = 1;
 		player.hp = 10;
 		player.score = 0;
-		player.speed = 0.0f;
+		player.speed = { 0.0f, 0.0f };
+		player.currentSpeed = 30.0f;
 		player.maxSpeed = 100.0f;
-		player.acceleration = 30.0f;
 		player.rotation = 0.0f;
 		player.pos.x = static_cast<float>(screenWidth / 2);
 		player.pos.y = static_cast<float>(screenHeight / 2);
@@ -54,18 +54,18 @@ namespace space_ore
 
 		Rectangle sourceDest = {player.pos.x, player.pos.y, 32, 32};
 		
-		DrawTexturePro(player.Spaceship, defaultShipState, sourceDest, player.pivot, player.rotation, WHITE);
+		DrawTexturePro(player.Spaceship, defaultShipState, sourceDest, player.pivot, (player.rotation - 270.0f), WHITE);
 	
 		
 		if (IsMouseButtonDown(MOUSE_BUTTON_RIGHT))
 		{
-			if (player.speed > 100.0f)
+			if (Vector2Length(player.speed) > 100)
 			{
-				DrawTexturePro(player.Spaceship, dashingShipState, sourceDest, player.pivot,player.rotation, WHITE);
+				DrawTexturePro(player.Spaceship, dashingShipState, sourceDest, player.pivot, (player.rotation - 270.0f), WHITE);
 			}
 			else
 			{
-				DrawTexturePro(player.Spaceship, movingShipState, sourceDest, player.pivot, player.rotation, WHITE);
+				DrawTexturePro(player.Spaceship, movingShipState, sourceDest, player.pivot, (player.rotation - 270.0f), WHITE);
 			}
 
 
@@ -80,48 +80,41 @@ namespace space_ore
 	{
 		Vector2 mousePos = GetMousePosition();
 		Vector2 rotation = { mousePos.x - player.pos.x,mousePos.y - player.pos.y };
-		Vector2 direction = { 0.0f,0.0f };
-		Vector2 directionNorm = { 0.0f,0.0f };
+		Vector2 direction = Vector2Subtract(mousePos, player.pos);
+		
 
-		//double quadrantSum = 0.0;
-
-		/*if (mousePos.x < player.pos.x)
-		{
-			
-				quadrantSum = 180.0;
-			
-		}
-		if (mousePos.x > player.pos.x )
-		{
-			if (mousePos.y < player.pos.y)
-			{
-				quadrantSum = 360.0;
-			}
-			else if (mousePos.y > player.pos.y)
-			{
-				quadrantSum = 0.0;
-			}
-		}*/
-
-		float angle = ((atan2(rotation.y, rotation.x)));
-		angle = (angle * 180 / PI);
-		//angle += quadrantSum;
-
-		player.rotation = angle - 270.0f;
+		
+		player.rotation = (atan2f(rotation.y, rotation.x) * RAD2DEG) ;
 
 
 		if (IsMouseButtonDown(MOUSE_BUTTON_RIGHT))
 		{
-			 direction = { mousePos.x - player.pos.x,mousePos.y - player.pos.y };
-			 directionNorm = Vector2Normalize(direction);
+			Vector2 acceleration = Vector2Scale(Vector2Normalize(direction), player.currentSpeed * GetFrameTime());
+			
 
-			player.speed += player.acceleration * GetFrameTime();
+			player.speed = Vector2Add(player.speed, acceleration);
+			
 
 		}
 
-		player.pos.x += directionNorm.x * GetFrameTime() * player.speed;
-		player.pos.y += directionNorm.y * GetFrameTime() * player.speed;
+		if (Vector2Length(player.speed) > player.maxSpeed)
+		{
+			player.speed = Vector2Scale(Vector2Normalize(player.speed), player.maxSpeed);
+		}
 
+		player.pos = Vector2Add(player.pos, Vector2Scale(player.speed, GetFrameTime()));
+
+		if (player.pos.x < -player.radius)
+			player.pos.x = static_cast<float>(GetScreenWidth()) + player.radius;
+
+		if (player.pos.x > GetScreenWidth() + player.radius)
+			player.pos.x = -player.radius;
+
+		if (player.pos.y < -player.radius)
+			player.pos.y = static_cast<float>(GetScreenHeight()) + player.radius;
+
+		if (player.pos.y > GetScreenHeight() + player.radius)
+			player.pos.y = -player.radius;
 
 		return player;
 	}
--- src/headers/Player.h
+++ src/headers/Player.h
@@ -9,14 +9,14 @@ namespace space_ore
 		Texture2D Spaceship;
 		Vector2 pos;
 		Vector2 pivot;
+		Vector2 speed;
 		int hp;
 		int score;
 		int maxScore;
 		int maxBullets;
 		int currentBullets;
-		float speed;
 		float maxSpeed;
-		float acceleration;
+		float currentSpeed;
 		float radius;
 		float rotation;
 	};
